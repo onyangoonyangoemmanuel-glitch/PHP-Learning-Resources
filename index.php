a074cb8451f1658ac3df90afc92fb54c5f76303d
onyango <onyangoonyangoemmanuel@gmail.com>
2025-11-02 07:48:11 +0300

    Additional updates

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>My PHP Webpage</title>
 </head>
 
@@ -14,6 +17,24 @@
     <main>
 
         <p>This is the main content of the page.</p>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type="number" name="num01" placeholder="Number One">
+            <select name="operator">
+                <option value="add">+</option>
+                <option value="subtract">-</option>
+                <option value="multiply">*</option>
+                <option value="divide">/</option>
+            </select>
+            <input type="number" name="num02" placeholder="Number two">
+            <button>Calculate</button>
+        </form>
+
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                
+            }
+        ?>
 
         <?php
             /*
